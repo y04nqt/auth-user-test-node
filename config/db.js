cc45eaecf1013f817ef678a24caed789419f3585
Aaron <akr.git.receiver@gmail.com>
2016-09-15 23:50:15 -0400

    added a little log

--- config/db.js
+++ config/db.js
@@ -2,11 +2,13 @@ var mongoose = require('mongoose');
 var connectionString = 'mongodb://localhost/user-auth';
 if (process.env.NODE_ENV === 'production') {
     connectionString = process.env.MONGOLAB_URI;
+    console.log(connectionString);
 }
 mongoose.connect(connectionString);
 
 mongoose.connection.on('connected', function() {
   console.log('Securing');
+  console.log(connectionString);
 });
 
 
